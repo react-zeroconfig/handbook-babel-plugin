d0bb4574687160a2daabebbb79f6db6d8ddfd130
SSen <i@ssen.name>
2020-08-28 00:30:01 +0900

    fix: add @handbook/* to moduleNameMapper for jest coverage

--- source/jest.config.js
+++ source/jest.config.js
@@ -2,6 +2,10 @@ const jestPresets = require('rocket-punch/jest-preset');
 
 module.exports = {
   ...jestPresets,
-
+  
   // customize your config
+  moduleNameMapper: {
+    ...jestPresets.moduleNameMapper,
+    '@handbook/(.*)': '<rootDir>/src/@handbook/$1',
+  },
 };
