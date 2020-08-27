9c9db3e5f05ef4aa2c06403b49d8abf335a3c010
SSen <i@ssen.name>
2020-08-28 01:22:15 +0900

    docs: add README

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,3 +1,7 @@
+function cp(from, to) {
+  return [`cp ${from} ${to}`, `git add ${to}`];
+}
+
 function precommit(...workspaces) {
   return workspaces.map((workspace) => `yarn workspace ${workspace} run precommit`);
 }
@@ -6,6 +10,9 @@ module.exports = {
   hooks: {
     'pre-commit': [
       `markdown-source-import "{,!(node_modules)/**/}*.md" --git-add`,
+      ...cp(`README.md`, `source/src/@handbook/babel-plugin/README.md`),
+      ...cp(`README.md`, `source/src/@handbook/source/README.md`),
+      ...cp(`README.md`, `source/src/@handbook/typescript-source-sampler/README.md`),
       `lint-staged`,
       ...precommit(`source`),
     ].join(' && '),
